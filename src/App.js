a6cd640a996303ed9c346a488f3aec6edec0cb3d
Wouter de Vos <wouter@codaisseur.com>
2016-07-06 14:06:42 +0200

    Add Roboto font and injectTapEventPlugin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import NewsFeed from './NewsFeed';
+
+injectTapEventPlugin();
 
 const feedUrl = "http://thenextweb.com/feed/";
 
